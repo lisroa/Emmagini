dc0fa8a03ae92a9124db3ddf19b81e9b91b58597
Lis <lisgimenaroa@gmail.com>
2024-08-08 11:10:02 -0300

    ajustes google login

--- app/auth/GoogleLoginButton.jsx
+++ app/auth/GoogleLoginButton.jsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import { useCallback, useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -64,6 +64,85 @@ const GoogleLoginButton = () => {
 			onSuccess={handleGoogleLoginSuccess}
 			onError={() => {
 				console.log("Login failed");
+			}}
+			className="border border-sky-700 rounded-full w-full h-[58px]"
+			theme="outline"
+			size="large"
+			shape="rectangular"
+			text="continue_with"
+			logo_alignment="center"
+		/>
+	);
+};
+
+export default GoogleLoginButton; */
+
+"use client";
+
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
+import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useAuthContext } from "@/app/context/AuthProvider";
+
+const GoogleLoginButton = () => {
+	const router = useRouter();
+	const { setToken, setUserId } = useAuthContext();
+
+	const TOKEN_KEY = "token";
+	const USER_ID_KEY = "user_id";
+
+	const handleGoogleLoginSuccess = useCallback(
+		async (credentialResponse) => {
+			try {
+				const response = await axios.post(
+					"https://backend.emmagini.com/api2/google_login",
+					{
+						host: "demo5.emmagini.com",
+						client_id: credentialResponse.clientId,
+						credential: credentialResponse.credential,
+						fcm_token: "",
+						es_app: "0",
+						id_plataforma: "3",
+						lang: "es",
+						timezone: "-3",
+					},
+					{
+						headers: {
+							"Content-Type":
+								"application/x-www-form-urlencoded; charset=UTF-8",
+						},
+					}
+				);
+
+				const { token, userid, error, mensaje } = response.data;
+
+				console.log("Datos recibidos:", response.data);
+
+				if (error !== 0) {
+					console.error("Error en la respuesta de login:", mensaje);
+					return;
+				}
+
+				localStorage.setItem(TOKEN_KEY, token);
+				localStorage.setItem(USER_ID_KEY, userid);
+
+				setToken(token);
+				setUserId(userid);
+
+				router.push("/app");
+			} catch (error) {
+				console.error("Error al hacer la solicitud:", error);
+			}
+		},
+		[router, setToken, setUserId]
+	);
+
+	return (
+		<GoogleLogin
+			onSuccess={handleGoogleLoginSuccess}
+			onError={() => {
+				console.error("Login failed");
 			}}
 			className="border border-sky-700 rounded-full w-full h-[58px]"
 			theme="outline"
