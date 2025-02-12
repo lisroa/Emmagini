fba74ac128028d98aa696eed46f8341277b843bf
Lis <lisgimenaroa@gmail.com>
2025-02-12 14:43:09 -0300

    fixxx

--- app/app/truco/jugar-partida/layout.tsx
+++ app/app/truco/jugar-partida/layout.tsx
@@ -1,18 +1,19 @@
-import {PartidasTrucoProvider} from "@/app/context/truco/PartidasTrucoProvider"
-import { SeriesTrucoProvider } from "@/app/context/truco/SeriesTrucoProvider";
-
+//import {PartidasTrucoProvider} from "@/app/context/truco/PartidasTrucoProvider"
+//import { SeriesTrucoProvider } from "@/app/context/truco/SeriesTrucoProvider";
 
 export default function Layout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-   
-    return (
-     <SeriesTrucoProvider>
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		/*<SeriesTrucoProvider>
        <PartidasTrucoProvider>
         {children}
       </PartidasTrucoProvider>
-     </SeriesTrucoProvider>
-    );
-  }
+     </SeriesTrucoProvider>*/
+		<p className="mt-20 text-black">
+			Page en construccion. Disculpe las molestias
+		</p>
+	);
+}
